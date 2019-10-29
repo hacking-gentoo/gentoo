bf57ae8daeb8a75035410219f13a27233b7122b0
Max Hacking <max@hacking.co.uk>
2019-10-29 18:28:42 +0000

    Switched to new overlay, added dev-util/codecov-bash to installed utils.

--- root.test/usr/local/sbin/install-test-tools.sh
+++ root.test/usr/local/sbin/install-test-tools.sh
@@ -7,11 +7,13 @@ emerge dev-vcs/git
 
 mkdir -p /etc/portage/repos.conf
 
-add_overlay hacking-gentoo https://github.com/MADhacking/hacking-gentoo.git
+add_overlay hacking-gentoo https://github.com/hacking-gentoo/overlay.git
 
+update_keywords 'dev-util/codecov-bash' '+~amd64'
 update_keywords 'dev-util/kcov' '+~amd64'
 
 emerge app-portage/repoman \
+       dev-util/codecov-bash \
        dev-util/kcov \
        dev-util/shellcheck-bin
 
