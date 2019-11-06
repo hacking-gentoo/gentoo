cf3c197eaa1d6bcfc076518a1a02aa34a36d2d62
Max Hacking <max@hacking.co.uk>
2019-11-06 13:30:29 +0000

    Don't run egencache - it's too slow!

--- root.base/usr/local/sbin/update-image.sh
+++ root.base/usr/local/sbin/update-image.sh
@@ -6,7 +6,7 @@ etc-update --automode -5
 emerge app-portage/eix app-portage/flaggie app-portage/gentoolkit
 emerge --depclean
 revdep-rebuild
-egencache --update --tolerant --repo gentoo --jobs 4
+#egencache --update --tolerant --repo gentoo --jobs 4
 eix-update
 rm /var/cache/distfiles/* -rf
 rm /usr/local/sbin/update-image.sh
