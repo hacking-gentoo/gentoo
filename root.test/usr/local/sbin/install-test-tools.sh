012fdd5570310301998483c530d5b149e86dfa63
Max Hacking <max@hacking.co.uk>
2019-11-06 13:31:38 +0000

    Add app-misc/jq to test-runner image.

--- root.test/usr/local/sbin/install-test-tools.sh
+++ root.test/usr/local/sbin/install-test-tools.sh
@@ -12,7 +12,8 @@ add_overlay hacking-gentoo https://github.com/hacking-gentoo/overlay.git
 update_keywords 'dev-util/codecov-bash' '+~amd64'
 update_keywords 'dev-util/kcov' '+~amd64'
 
-emerge app-portage/repoman \
+emerge app-misc/jq \
+       app-portage/repoman \
        dev-perl/File-MimeInfo \
        dev-util/codecov-bash \
        dev-util/kcov \
