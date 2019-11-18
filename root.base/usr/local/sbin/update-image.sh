b134f35bad0916846408cedc8d77c603a5b2967f
Max Hacking <max@hacking.co.uk>
2019-11-18 18:02:09 +0000

    Use directory for make.conf

--- root.base/usr/local/sbin/update-image.sh
+++ root.base/usr/local/sbin/update-image.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+mv /etc/portage/make.conf ~/
+mkdir /etc/portage/make.conf
+mv ~/make.conf /etc/portage/make.conf/00-gentoo-defaults
+mv /etc/portage/make.extra /etc/portage/make.conf/01-hacking-defaults
+mv /etc/portage/make.binpkg /etc/portage/make.conf/02-hacking-binpkg
+
 emerge -uDN world
 etc-update --automode -5
 emerge app-portage/eix app-portage/flaggie app-portage/gentoolkit
