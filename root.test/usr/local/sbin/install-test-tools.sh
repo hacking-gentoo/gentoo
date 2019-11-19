53a8c8fb87db7968f93d841ab29e1fdfd72788b7
Max Hacking <max@hacking.co.uk>
2019-11-19 01:50:02 +0000

    Clean any unused binary packages after update.

--- root.test/usr/local/sbin/install-test-tools.sh
+++ root.test/usr/local/sbin/install-test-tools.sh
@@ -24,6 +24,9 @@ emerge app-misc/jq \
        dev-util/shellcheck-bin \
        net-libs/libsmi
 
+# Clean any unused binary packages to stop the cache getting polluted.
+qpkg --clean --nocolor
+
 # Don't default to building or consuming binary packages
 rm /etc/portage/make.conf/02-hacking-binpkg
 
